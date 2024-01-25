0e73164a93aeb5bb1ee55a234768e2753d901406
seinnlet <sllhninn.@gmail.com>
2024-01-26 00:12:12 +0900

    updated

--- Shiritori/Shiritori.java
+++ Shiritori/Shiritori.java
@@ -12,8 +12,33 @@ public class Shiritori {
 	}
 	
 	static void initial() {
-		System.out.println("しりとりゲーム（終わりたい場合はEndと入力）");
-		System.out.println("----------------------------");
+		System.out.println("しりとり");
+		System.out.println("----");
+	}
+	
+	/*
+	 * 「しりとり」 という言葉から始まり、プレイヤーが 「end」 書くまで遊ぶ
+	 * 
+	 * @param String	前回入力した言葉
+	 */
+	static void play(String currentWord) {
+		int i = 1;
+		System.out.print(currentWord + "\n" + i + "回目 「");	
+		String newWord = inputWord(getLastCharacter(currentWord));
+		
+		while(getLastCharacter(newWord) != 'ん') {
+			
+			if(checkCharacter(getFirstCharacter(newWord), getLastCharacter(currentWord))) {
+				currentWord = newWord;
+				i++;
+			} else {
+				System.out.println("違います。もう一回入力してください。	");
+			}
+			System.out.print(i + "回目 「");
+			newWord = inputWord(getLastCharacter(currentWord));
+		}
+		
+		System.out.println("負けました。「ん」で終わってはいけません。");
 	}
 	
 	/*
@@ -23,7 +48,12 @@ public class Shiritori {
 	 * @return char		前回の言葉の最後の文字
 	 */
 	static char getLastCharacter(String str) {
-		return str.charAt(str.length()-1);
+		char lastChar = toUpperCharacter(str.charAt(str.length()-1));
+		
+		if (lastChar == 'ー') {
+			lastChar = changeLongVowel(toUpperCharacter(str.charAt(str.length()-2)));
+		}
+		return lastChar;
 	}
 	
 	/*
@@ -72,25 +102,29 @@ public class Shiritori {
 		}
 	}
 	
-	/*
-	 * 「しりとり」 という言葉から始まり、プレイヤーが 「end」 書くまで遊ぶ
-	 * 
-	 * @param String	前回入力した言葉
-	 */
-	static void play(String currentWord) {
-		int i = 1;
-		System.out.print(currentWord + "\n" + i + "回目 「");	
-		String newWord = inputWord(getLastCharacter(currentWord));
-		
-		while(!newWord.toLowerCase().equals("end")) {
-			if(checkCharacter(getFirstCharacter(newWord), getLastCharacter(currentWord))) {
-				currentWord = newWord;
-				i++;
-			} else {
-				System.out.println("違います。もう一回入力してください。	");
-			}
-			System.out.print(i + "回目 「");
-			newWord = inputWord(getLastCharacter(currentWord));
-		}
+	static char toUpperCharacter(char lastChar) {
+		return switch(lastChar) {
+			case 'ゃ' -> 'や';
+			case 'ゅ' -> 'ゆ';
+			case 'ょ' -> 'よ';
+			case 'ぁ' -> 'あ';
+			case 'ぃ' -> 'い';
+			case 'ぅ' -> 'う';
+			case 'ぇ' -> 'え';
+			case 'ぉ' -> 'お';
+			default -> lastChar;
+		};
 	}
+	
+	static char changeLongVowel(char lastChar) {
+		return switch(lastChar) {
+		case 'か', 'さ', 'た', 'な', 'は', 'ま', 'や', 'ら', 'わ', 'が', 'ざ', 'だ', 'ば', 'ぱ' -> 'あ';
+		case 'き', 'し', 'ち', 'に', 'ひ', 'み', 'り', 'ぎ', 'じ', 'ぢ', 'び', 'ぴ' -> 'い';
+		case 'く', 'す', 'つ', 'ぬ', 'ふ', 'む', 'ゆ', 'る', 'ぐ', 'ず', 'づ', 'ぶ', 'ぷ' -> 'う';
+		case 'け', 'せ', 'て', 'ね', 'へ', 'め', 'れ', 'げ', 'ぜ', 'で', 'べ', 'ぺ' -> 'え';
+		case 'こ', 'そ', 'と', 'の', 'ほ', 'も', 'よ', 'ろ', 'を', 'ご', 'ぞ', 'ど', 'ぼ', 'ぽ' -> 'お';
+		default -> lastChar;
+	};
+	}
+	
 }
